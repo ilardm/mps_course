ca1e780de9ab2a9c8f2814e97805be034dbced3e
Ilya Arefiev <arefiev.id@gmail.com>
2013-03-17 13:49:55 +0400

    implemented c fibonacci

--- 01_fibonacci/c/src/main.c
+++ 01_fibonacci/c/src/main.c
@@ -2,7 +2,26 @@
 
 #include        "main.h"
 
+static const int FIBN = 10;
+
 int main(int argc, char** argv)
 {
+    int f[2] = {0, 1};
+    int fn = 0;
+
+    int i = 0;
+    for ( i = 0; i < FIBN; ++i ) {
+        if ( i < 2 ) {
+            fn = f[i];
+        } else {
+            fn = f[0] + f[1];
+
+            f[0] = f[1];
+            f[1] = fn;
+        }
+
+        printf( "fn[%d] = %d\n", i+1, fn );
+    }
+
     return 0;
 }
