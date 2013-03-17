3c644cfa4e59d2d19e5c1bf2782fd43cb9f77863
Ilya Arefiev <arefiev.id@gmail.com>
2013-03-17 15:27:51 +0400

    implemented fermat c

--- 06_fermat/c/src/main.c
+++ 06_fermat/c/src/main.c
@@ -2,7 +2,18 @@
 
 #include        "main.h"
 
+static const int FERMATN = 5;
+
 int main(int argc, char** argv)
 {
+    int i = 0;
+    int fn = 0;
+
+    for ( i = 0; i < FERMATN; ++i ) {
+        fn = ( 1 << (1 << i) ) + 1;
+
+        printf( "fn[%d] = %d\n", i+1, fn );
+    }
+
     return 0;
 }
