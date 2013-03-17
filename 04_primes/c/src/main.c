e76d943616f2599220e7514076aba914fda8da7a
Ilya Arefiev <arefiev.id@gmail.com>
2013-03-17 15:08:24 +0400

    implemented primes c

--- 04_primes/c/src/main.c
+++ 04_primes/c/src/main.c
@@ -2,7 +2,31 @@
 
 #include        "main.h"
 
+#define NLIMIT 1000
+static const int PRIMESN = 10;
+static const int FIRST_PRIME = 2;
+
+typedef enum  PCNUMBERS_ENUM { PRIME, COMPOSITE } PCNUMBERS;
+
 int main(int argc, char** argv)
 {
+    /* Sieve of Eratosthenes */
+    PCNUMBERS sieve[ NLIMIT+1 ] = { PRIME };
+
+    int i = 0;                  /* primes iterator */
+    int c = 0;                  /* composites iterator */
+    int count = 0;              /* primes count */
+
+    for ( i = FIRST_PRIME; i < NLIMIT; ++i )
+        if ( sieve[i] == PRIME )
+            for ( c = i*i; c < NLIMIT; c += i )
+                sieve[c] = COMPOSITE;
+
+    for ( i = FIRST_PRIME, count = 0; i < NLIMIT && count < PRIMESN ; ++i )
+        if ( sieve[i] == PRIME ) {
+            printf( ( count < PRIMESN-1 ? "%d, " : "%d\n" ), i );
+            ++count;
+        }
+
     return 0;
 }
