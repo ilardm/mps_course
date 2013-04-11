76d6ad94d598c36bdf2844bf52b098e8148da873
Ilya Arefiev <arefiev.id@gmail.com>
2013-04-11 19:50:58 +0400

    implemented lucas asm

--- 02_lucas/asm/src/main.s
+++ 02_lucas/asm/src/main.s
@@ -1,10 +1,34 @@
-.text
+.section .data
+        .equ    lucn,10                 @ length of Luc. sequence
 
-.global main
+.section .bss
+        .lcomm  luc,40                  @ array for Luc. sequence
+
+.section .text
+
+        .global main
 main:
         push    {ip,lr}
 
-        @ actual code here
+        @ calculate end address of luc array
+        mov     r0,#lucn
+        mov     r1,#4
+        mul     r4,r0,r1
+        ldr     r0,=luc
+        add     r4,r4,r0
+
+        @ initialize first two numbers
+        mov     r1,#2
+        mov     r2,#1
+        stmia   r0!,{r1,r2}
+
+        @ fill fib array
+loop:
+        ldmdb   r0,{r2,r1}
+        add     r3,r2,r1
+        str     r3,[r0],#4
+        cmp     r0,r4
+        blt     loop
 
         mov     r0,#0
         pop     {ip,pc}
