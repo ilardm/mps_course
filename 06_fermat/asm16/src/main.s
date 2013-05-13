910a5beab827481804042699fa834cbd6cd58554
Ilya Arefiev <arefiev.id@gmail.com>
2013-05-13 21:52:09 +0400

    copied asm sources

--- 06_fermat/asm16/src/main.s
+++ 06_fermat/asm16/src/main.s
@@ -1,4 +1,11 @@
 .thumb
+.data
+        .set    bypv,4
+        .set    fermn,5
+
+ferm:   .size   ferm,bypv*fermn
+        .fill   fermn,bypv,0
+
 .text
         bal     main
 
@@ -7,7 +14,22 @@
 main:
         push    {r4-r7,lr}
 
-        @ actual code here
+        ldr     r4,=ferm
+        mov     r5,#0                   @ i
+        mov     r7,#1                   @ aux register
+
+loop:
+        mov     r6,r7,lsl r5
+        mov     r6,r7,lsl r6
+        add     r6,r6,#1
+        str     r6,[r4],#bypv
+
+        add     r5,r5,#1
+        cmp     r5,#fermn
+        blt     loop
+
+done:
+        ldr     r4,=ferm
         
         mov     r0,#0
         pop     {r4-r7,pc}
