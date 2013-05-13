03d90121369e5008b3930e5ce4e51d0b9c04fe66
Ilya Arefiev <arefiev.id@gmail.com>
2013-05-13 22:10:30 +0400

    implemented fermat asm16

--- 06_fermat/asm16/src/main.s
+++ 06_fermat/asm16/src/main.s
@@ -16,13 +16,15 @@ main:
 
         ldr     r4,=ferm
         mov     r5,#0                   @ i
-        mov     r7,#1                   @ aux register
 
 loop:
-        mov     r6,r7,lsl r5
-        mov     r6,r7,lsl r6
+        mov     r7,#1
+        lsl     r7,r5
+        mov     r6,#1
+        lsl     r6,r7
         add     r6,r6,#1
-        str     r6,[r4],#bypv
+        str     r6,[r4,#0]
+        add     r4,#bypv
 
         add     r5,r5,#1
         cmp     r5,#fermn
@@ -30,7 +32,7 @@ loop:
 
 done:
         ldr     r4,=ferm
-        
+
         mov     r0,#0
         pop     {r4-r7,pc}
 
