7efcdabe69f3f7872a78975af664ea74972db30f
Ilya Arefiev <arefiev.id@gmail.com>
2013-05-25 10:28:26 +0400

    modifed fibonacci c

--- 01_fibonacci/c/src/main.c
+++ 01_fibonacci/c/src/main.c
@@ -2,25 +2,16 @@
 
 #include        "main.h"
 
-static const int FIBN = 10;
+#define FIBN    (10)
 
 int main(int argc, char** argv)
 {
-    int f[2] = {0, 1};
-    int fn = 0;
-
+    int f[FIBN] = {0};
     int i = 0;
-    for ( i = 0; i < FIBN; ++i ) {
-        if ( i < 2 ) {
-            fn = f[i];
-        } else {
-            fn = f[0] + f[1];
 
-            f[0] = f[1];
-            f[1] = fn;
-        }
-
-        printf( "fn[%d] = %d\n", i+1, fn );
+    f[1] = 1;
+    for ( i = 2; i < FIBN; ++i ) {
+        f[i] = f[i-1] + f[i-2];
     }
 
     return 0;
